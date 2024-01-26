b193286706e65d14d8050ed121ddd3b875efe20f
brobustert <157801311+brobustert@users.noreply.github.com>
2024-01-26 21:06:47 +0900

    Add files via upload

---
+++ prom.js
@@ -0,0 +1,18 @@
+function smoothScroll(target) {
+    document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
+}
+
+
+function submitForm(event) {
+    event.preventDefault();
+    
+    alert('Form submitted successfully!');
+}
+
+
+
+
+
+window.onload = function() {
+    
+    alert("Welcome to Amir's Portfolio!")}
